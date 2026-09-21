6643588bbbc9a6568e0b4b9f51e994ae7fcd5725
gamp <bramtechxxvi@gmail.com>
2026-09-21 15:37:34 +0100

    test solve bard returns false for invalid board

--- backend/tests/services/test_services.py
+++ backend/tests/services/test_services.py
@@ -193,3 +193,21 @@ def test_solve_board_returns_false_for_unsolvable_board():
         [0, 0, 0, 0, 8, 0, 0, 7, 9],
     ]
     assert solve_board(board) is False
+    
+    
+def test_solve_board_returns_false_for_invalid_board():
+    board = [
+        [5, 5, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+    result = solve_board(board)
+    assert result is False
