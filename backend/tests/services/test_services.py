ff11d394e9e363df5020886bba6cf9df0c66b352
gamp <bramtechxxvi@gmail.com>
2026-09-22 10:02:13 +0100

    test is coplete board returns true for a completed valid board passes

--- backend/tests/services/test_services.py
+++ backend/tests/services/test_services.py
@@ -422,3 +422,21 @@ def test_is_complete_returns_true_for_a_completed_valid_board():
     ]
     result = is_complete(board)
     assert result is True
+    
+    
+
+def test_is_complete_returns_false_when_board_has_empty_cells():
+    board = [
+        [5, 3, 4, 0, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9],
+    ]
+    assert is_complete(board) is False
