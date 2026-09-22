14dc73982dfa7aee46c17d91bc0301e484ddc2a0
gamp <bramtechxxvi@gmail.com>
2026-09-22 14:27:55 +0100

    updated soduko schem with required serializers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from app.routes.sudoku_router import router as routes
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import ALLOWED_ORIGINS
+
 
 app = FastAPI(
     title="Sudoku API",
     version= "1.0.0"
+)
+
+app.add.middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 app.include_router(routes)
 
