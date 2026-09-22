1b55b733ab0b4cd6bb9b3afad95bd88785ad0e33
gamp <bramtechxxvi@gmail.com>
2026-09-22 10:04:12 +0100

    test is comlete board return for full invalid board

--- backend/tests/services/test_services.py
+++ backend/tests/services/test_services.py
@@ -444,4 +444,17 @@ def test_is_complete_returns_false_when_board_has_empty_cells():
 
 
 def test_is_complete_false_for_full_invalid_board():
-    pass
+    board = [
+        [5, 5, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9],
+    ]
+    assert is_complete(board) is False
