0c3ef1cf84a6227a32e71fa28fe85d48859fe44e
gamp <bramtechxxvi@gmail.com>
2026-09-22 11:26:38 +0100

    test endpoint chck imcomplete board

--- backend/tests/routes/test_sudoku_routes.py
+++ backend/tests/routes/test_sudoku_routes.py
@@ -49,3 +49,32 @@ def test_make_valid_move():
     data = response.json()
     assert data["valid"] is True
     assert data["board"][0][2] == 4 
+    
+    
+
+def test_check_incomplete_board():
+    response = client.post(
+        f"{BASE_URL}/check", 
+        json={"board": BOARD})
+    
+    assert response == 200
+    data = response.json()
+    
+    assert data["valid"] is True
+    assert data["complete"] is False
+    
+    
+    
+# def test_hint_returns_hint():
+#     response = client.post(
+#         f"{BASE_URL}/hint",
+#         json={ "board": BOARD, }
+#     )
+#     response.status_code == 200
+#     data = response.json()
+    
+#     assert data["hint"] == {
+#         "row": 0,
+#         "col": 2,
+#         "number": 4
+#     }
