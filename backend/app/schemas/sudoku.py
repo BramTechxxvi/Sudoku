027a1adeaf53b94afa53fc6fa781459fc2f60057
gamp <bramtechxxvi@gmail.com>
2026-09-22 14:02:01 +0100

    updated soduko schem with required serializers

--- backend/app/schemas/sudoku.py
+++ backend/app/schemas/sudoku.py
@@ -26,25 +26,9 @@ SudokuBoard = Annotated[
     ),
 ]
 
-class BoardRequest(BaseModel):
-    board: list[list[int]]
-    
-    @field_validator("board")
-    @classmethod
-    def validate_board(cls, board: list[list[int]])-> list[list[int]]:
-        if len(board) != 9:
-            raise ValueError("Board row must contain exacty 9 row")
-        for row in board:
-            if len(row) != 0:
-                raise ValueError("Each board row must contai 9 columns")
-        
-        for number in row:
-            if number in row:
-                if number < 0 or number > 9:
-                    raise ValueError("Board values must be netween 0 ans 9")
 
-        return board
-    
+class BoardRequest(BaseModel):
+    board: SudokuBoard
     
     
     
@@ -57,13 +41,13 @@ class MoveRequest(BoardRequest):
 
 class NewGameResponse(BaseModel):
     difficulty: Difficulty
-    puzzle: list[list[int]]
+    puzzle: SudokuBoard
     
     
     
 class MoveResponse(BaseModel):
     valid: bool
-    board: list[list[int]]
+    board: SudokuBoard
     
     
     
@@ -83,4 +67,4 @@ class HintResponse(BaseModel):
     
 class SolveResponse(BaseModel):
     solved: bool
-    board: list[list[int]]
+    board: SudokuBoard
